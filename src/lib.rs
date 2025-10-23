4cf4ab1ef0526a36ff3059b0015314e801fb8a9a
michaellynch <michael@supermarinesoftware.com>
2025-10-23 12:39:51 -0500

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use std::fmt::format;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    unsafe { alert(&format!("Hello {}!", name)); }
+}
