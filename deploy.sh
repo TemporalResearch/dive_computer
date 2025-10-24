3915167150124ac366918976df200e478bc6efdf
michaellynch <michael@supermarinesoftware.com>
2025-10-23 20:53:56 -0500

    start creating tissue compartment code

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+wasm-pack build --target web --out-dir ../../html/webapps/dive_computer/src
+cp resources/* ../../html/webapps/dive_computer/
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,119 @@
-use std::fmt::format;
+use std::cmp::min;
+use std::fmt::{Display, Formatter};
+use std::ops::Add;
 use wasm_bindgen::prelude::wasm_bindgen;
+use web_sys::{window, Window, Document};
 
+#[wasm_bindgen]
 extern "C" {
     pub fn alert(s: &str);
+
+    pub fn console_log(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    unsafe { alert(&format!("Hello {}!", name)); }
+    console_log("Hello there");
+
+    let window: Window = window().unwrap();
+    let document: Document = window
+        .document()
+        .unwrap();
+
+    console_log("Hello there 2");
+
+    console_log("Hello there 3");
+    document.get_element_by_id("nitro_cont")
+        .unwrap()
+        .set_inner_html("0.79");
+
+    console_log("Hello there 4");
+}
+
+const AIR_NITROGEN_RATIO: f32 = 0.79;
+const WATER_VAPOUR_PRESSURE: f32 = 0.063;
+
+#[wasm_bindgen]
+#[derive(Copy, Clone, PartialOrd, PartialEq)]
+pub struct Ata(f32);
+#[wasm_bindgen]
+pub fn js_ata(ata: f32) -> Ata {
+    Ata(ata)
+}
+
+impl Add for Ata {
+    type Output = Ata;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0)
+    }
+}
+
+impl Display for Ata {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("{}ata", self.0))
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Copy, Clone, PartialOrd, PartialEq)]
+pub struct Minutes(f32);
+#[wasm_bindgen]
+pub fn js_minutes(minutes: f32) -> Minutes {
+    Minutes(minutes)
+}
+
+#[wasm_bindgen]
+#[derive(Copy, Clone, PartialOrd, PartialEq)]
+pub struct Feet(f32);
+#[wasm_bindgen]
+pub fn js_feet(feet: f32) -> Feet {
+    Feet(feet)
+}
+
+impl Feet {
+    fn depth_atmospheric_pressure(&self) -> Ata {
+        Ata(1f32 + (self.0 / 33f32))
+    }
+}
+
+#[wasm_bindgen]
+pub struct TissueCompartment {
+    // constants
+    surface_m_value: Ata,
+    m_slope: f32,
+    half_time: Minutes,
+
+    nitrogen_concentration: Ata,
+}
+
+#[wasm_bindgen]
+impl TissueCompartment {
+    #[wasm_bindgen(constructor)]
+    pub fn new(half_time: f32, surface_m_value: f32, m_slope: f32) -> TissueCompartment {
+        TissueCompartment {
+            surface_m_value: Ata(surface_m_value),
+            m_slope,
+            half_time: Minutes(half_time),
+
+            nitrogen_concentration: Ata(AIR_NITROGEN_RATIO), // Start at sea level nitrogen pressure
+        }
+    }
+
+    pub fn simulate(&mut self, time_at_depth: Minutes, depth: Feet, nitrogen_ratio: f32) {
+        let inspired_pressure = (depth.depth_atmospheric_pressure().0 - WATER_VAPOUR_PRESSURE) * nitrogen_ratio;
+        let k = 2f32.ln() / self.half_time.0;
+
+        self.nitrogen_concentration = Ata(
+            self.nitrogen_concentration.0
+            + (
+                (inspired_pressure - self.nitrogen_concentration.0)
+                * (1f32 - std::f32::consts::E.powf(-(k * time_at_depth.0)))
+            ));
+
+        window().unwrap().document().unwrap()
+            .get_element_by_id("nitro_cont")
+            .unwrap()
+            .set_inner_html(&format!("{}", self.nitrogen_concentration));
+    }
 }
